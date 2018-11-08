86d27f52f9d48a7d3682da36c5a7eb266d845ba1
Arthur Mauvezin <arthur.mauvezin@netapsys.fr>
2018-11-08 09:34:32 +0100

    Update project

--- app/back/webservice.py
+++ app/back/webservice.py
@@ -3,17 +3,30 @@ from aiohttp import web
 from datetime import datetime
 
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
+    name = request.match_info.get('name', "Here is the root")
     data = {
             'time': str(datetime.now()),
             'environment': os.environ['ENVIRONMENT'],
+            'hostname': os.uname()[1],
             'result': name
             }
     return web.json_response(data)
 
+async def write_to_file(request):
+    something = request.match_info.get('something', "")
+
+    print(something)
+    if something:
+        with open('logs/my-messages.log', 'a') as the_file:
+            the_file.write(f"{something}\n")
+    else:
+        return web.Response(status=500)
+
+    return web.Response(status=200)
+
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.get('/write/{something}', write_to_file)])
 
 web.run_app(app)
--- app/front/front.py
+++ app/front/front.py
@@ -6,21 +6,39 @@ import json
 
 async def handle(request):
     name = request.match_info.get('name', "")
-    url=f"http://back:8080/{name}"
-    #url=f"http://"+os.environ['WS_BACK']+":8080/{name}"
+
+    #url=f"http://back:8080/{name}"
+    url=f"http://{os.environ['WS_BACK_URL']}:8080/{name}"
     response = requests.get(url)
     if (response.ok):
         json_data = json.loads(response.content)
         resp = f"""
         You called at : {json_data['time']} (dynamic)
         On environment : {json_data['environment']} (from env variable)
-        With path : {json_data['result']}   (from path)
+        With path : {json_data['result']}   (from URL path)
+        With front : {os.uname()[1]} (from real hostname of front service)
+        With back  : {json_data['hostname']} (from real hostname of back service)
         """
     
     return web.Response(text=resp)
 
+async def write_to_file(request):
+    something = request.match_info.get('something', "")
+
+    print(something)
+    if something:
+        resp=something
+
+        url=f"http://{os.environ['WS_BACK_URL']}:8080/write/{something}"
+        response = requests.get(url)
+    else:
+        resp="There is nothing to print"
+
+    return web.Response(text=resp)
+
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.get('/write/{something}', write_to_file)])
 
 web.run_app(app, port=os.environ['APP_PORT'])
